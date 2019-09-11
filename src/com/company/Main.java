c4adb22e5f5b1669ef824bc294a708adbc799e67
elnaz123774 <s170301@student.dtu.dk>
2019-09-11 12:47:05 +0200

    nu med joptoinpane

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hej Elnaz");
+        System.out.println("Hej Elnazs");
+
+        JOptionPane.showMessageDialog(null,"Hej ");
 	// write your code here
     }
 }
